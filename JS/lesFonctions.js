b46840af28a1e711390685d2397f4ccee7c67fb3
thoma <thoma@172.16.0.68>
2017-10-20 18:40:04 +0200

    changement de v_Accueil par view_Accueil

--- JS/lesFonctions.js
+++ JS/lesFonctions.js
@@ -1,4 +1,24 @@
-function creerMedicament(){
+function creerMedicament()
+{
+    $Ajax
+    (
+            {
+                type="post",
+                url="creerMedicament",
+                data="depot="+$('#MED_DEPOTLEGAL').val()+" &nom="+$('#MED_NOMCOMMERCIAL').val()+" &code="+$('#FAM_CODE').val()+" &compo="+$('#MED_COMPOSITION').val()+" &effets="+$('#MED_EFFETS').val()+" &contreindic="+$('#MED_CONTREINDIC').val()+" &prix="+$('#MED_PRIXECHANTILLON').val(),
+                success:function(data)
+                {
+                    $('#div1').empty();
+                    $('#div1').append(data);
+                },
+                error:function()
+                {
+                    alert("Erreur de saisie");
+                }
+            }
+    )
+}
+function modifMedoc(){
     
 }
 
