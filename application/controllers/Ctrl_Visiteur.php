0a8471a7909d33813972df59ec11855b07808879
tiplo <tiplo@LAPTOP-885CLLGE>
2017-12-18 12:48:57 +0100

    fini

--- application/controllers/Ctrl_Visiteur.php
+++ application/controllers/Ctrl_Visiteur.php
@@ -4,6 +4,7 @@ class Ctrl_Visiteur extends CI_Controller{
     
        public function index(){
            
+           
        $this->load->model("Model_Visiteur"); 
        $data["lesVisiteurs"] = $this->Model_Visiteur->GetAllVisiteur();
        $this->load->view("v_Accueil",$data);
