44405ab660f9134b1d4441fa3e3f2c2af712cecb
Ivan Naluzhnyi <Ivan Naluzhnyi@DESKTOP-HO11IL4>
2017-12-18 20:36:52 +0100

    Merge

---
+++ application/views/v_Main.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>GSB</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+               
+                    <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+            <!-- Optional theme -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+
+        <div class="container">
+            <h1 class="text-left">GSB</h1>
+            <a href="<?php echo base_url(); ?>index.php/Ctrl_Composant_Ivan/index">Ivan</a><label> - Gestion des composants	</label><br>
+            <a href="<?php echo base_url(); ?>index.php/Ctrl_Visiteur/index">Sandra</a><label> - Gestion des visiteurs</label><br>
+            <a href="<?php echo base_url(); ?>index.php/Ctrl_Medoc/index">Thomas</a><label> - Gestion des médicaments	</label><br>
+            
+
+        </div>
+    </body>
+</html>
